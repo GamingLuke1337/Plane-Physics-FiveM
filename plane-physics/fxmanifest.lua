4ef4cd2830cdab9fda966331a8b38a0c99c15f3c
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2024-10-27 17:18:15 +0100

    Add files via upload

---
+++ plane-physics/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+author 'Entity Prime & Gamingluke1337'
+description 'Better Plane Physics'
+version '1.0.1'
+
+client_scripts {
+    'client/main.lua',
+}
+
